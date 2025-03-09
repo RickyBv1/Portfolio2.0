67d677118c669153aa4b92484164bbd6cd5b79d1
RickyBv1 <rickycortes668668@gmail.com>
2025-03-08 19:32:30 -0500

    update project structure and styles, remove favicon, and add new logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FlexLayoutModule } from '@ngbracket/ngx-layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FlexLayoutModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
