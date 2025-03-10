39e63ffe22d9ebf80c0baf124e00f90f1be1a45d
RickyBv1 <rickycortes668668@gmail.com>
2025-03-09 20:22:50 -0400

    enhance navigation and layout, add social media links, and improve menu functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FlexLayoutModule } from '@ngbracket/ngx-layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FlexLayoutModule],
+  imports: [RouterOutlet, FlexLayoutModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Portfolio2.0';
+
+  isShowMenu:boolean = true;
+
+  isHome:boolean = false;
+  isAboutMe:boolean = false;
+  isProjects:boolean = false;
+  isContact:boolean = false;
+
 }
